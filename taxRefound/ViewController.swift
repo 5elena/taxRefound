efc99eacf0bb22a48dfe07ef3596b4c122171678
Selena <selena@2001-b011-400a-1c02-10e2-4a53-d1c5-547b.dynamic-ip6.hinet.net>
2018-09-09 17:07:14 +0800

    done.

--- taxRefound/ViewController.swift
+++ taxRefound/ViewController.swift
@@ -9,12 +9,32 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var money: UITextField!
+    @IBOutlet weak var percentage: UITextField!
+    
+    @IBOutlet weak var refound: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
+    @IBAction func calculate(_ sender: UIButton) {
+        
+    //按下計算收鍵盤
+    money.resignFirstResponder()
+    percentage.resignFirstResponder()
+    
+    refound.isHidden = false
+    
+    //以下為防止有金額未輸入導致當掉
+    if money.text == "" || percentage.text == "" //如果總金額與退稅％數有任一未填
+    {
+    refound.text = "0" //則顯示為0
+    }else {let Refound = Double(money.text!)! * Double(percentage.text!)! / 100  //運算式
+    refound.text = String(Refound) //讓結果等於Refound這串運算式
+    }
 
 }
 
+}
